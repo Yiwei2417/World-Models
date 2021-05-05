32693d8a8b7d1abe682737466fe4e224d9396761
Jerry Lee <darkcb109xx@gmail.com>
2021-05-05 19:26:24 -0400

    added vaegan to series.py

--- WorldModels/series.py
+++ WorldModels/series.py
@@ -3,7 +3,8 @@ import os
 import json
 import tensorflow as tf
 import random
-from vae.vae import CVAE
+#from vae.vae import CVAE
+from vaegan.vaegan import VAEGAN
 from utils import PARSER
 
 gpu_devices = tf.config.experimental.list_physical_devices('GPU')
@@ -48,12 +49,12 @@ def create_tf_dataset():
 @tf.function
 def encode_batch(batch_img):
   simple_obs = batch_img/255.0
-  mu, logvar = vae.encode_mu_logvar(simple_obs)
+  mu, logvar = vaegan.encode(simple_obs)
   return mu, logvar
 
 def decode_batch(batch_z):
   # decode the latent vector
-  batch_img = vae.decode(z.reshape(batch_size, z_size)) * 255.
+  batch_img = vaegan.decode(z.reshape(batch_size, z_size)) * 255.
   batch_img = np.round(batch_img).astype(np.uint8)
   batch_img = batch_img.reshape(batch_size, 64, 64, 3)
   return batch_img
@@ -64,9 +65,56 @@ filelist = filelist[0:10000]
 dataset = create_tf_dataset()
 dataset = dataset.batch(1, drop_remainder=True)
 
-vae = CVAE(args=args)
+#vae = CVAE(args=args)
+N_Z = args.z_size
+encoder = [
+    tf.keras.layers.InputLayer(input_shape=(64, 64, 3)),
+    tf.keras.layers.Conv2D(
+            filters=32, kernel_size=3, strides=(2, 2), activation="relu"
+    ),
+    tf.keras.layers.Conv2D(
+        filters=64, kernel_size=3, strides=(2, 2), activation="relu"
+    ),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=N_Z*2),
+    ]
+decoder = [
+    tf.keras.layers.Dense(units=1 * 1 * 4*256, activation="relu"),
+    tf.keras.layers.Reshape(target_shape=(1 * 1 * 4*256)),
+    tf.keras.layers.Conv2DTranspose(
+        filters=64, kernel_size=3, strides=(2, 2), padding="SAME", activation="relu"
+    ),
+    tf.keras.layers.Conv2DTranspose(
+        filters=32, kernel_size=3, strides=(2, 2), padding="SAME", activation="relu"
+    ),
+    tf.keras.layers.Conv2DTranspose(
+        filters=1, kernel_size=3, strides=(1, 1), padding="SAME", activation="sigmoid"
+    ),
+]
+def vaegan_discrim():
+    inputs = tf.keras.layers.Input(shape=(64, 64, 3))
+    conv1 = tf.keras.layers.Conv2D(
+                filters=32, kernel_size=3, strides=(2, 2), activation="relu"
+            )(inputs)
+    conv2 = tf.keras.layers.Conv2D(
+                filters=64, kernel_size=3, strides=(2, 2), activation="relu"
+            )(conv1)
+    flatten = tf.keras.layers.Flatten()(conv2)
+    lastlayer = tf.keras.layers.Dense(units=512, activation="relu")(flatten)
+    outputs = tf.keras.layers.Dense(units=1, activation = None)(lastlayer)
+    return inputs, lastlayer, outputs
+vaegan = VAEGAN(
+    enc = encoder,
+    dec = decoder,
+    vae_disc_function = vaegan_discrim,
+    lr_base_gen = args.vae_learning_rate, 
+    lr_base_disc = args.vae_learning_rate,
+    latent_loss_div=1, # this variable will depend on your dataset - choose a number that will bring your latent loss to ~1-10
+    sig_mult = 10, # how binary the discriminator's learning rate is shifted (we squash it with a sigmoid)
+    recon_loss_div = .001, # this variable will depend on your dataset - choose a number that will bring your latent loss to ~1-10
+)
 
-vae.set_weights(tf.keras.models.load_model(model_path_name, compile=False).get_weights())
+vaegan.set_weights(tf.keras.models.load_model(model_path_name, compile=False).get_weights())
 mu_dataset = []
 logvar_dataset = []
 action_dataset = []
